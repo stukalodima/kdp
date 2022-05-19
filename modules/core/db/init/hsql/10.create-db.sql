4d89e88166d3a543b43b12be75474371e127ba2d
aleksandr.zhydok <aleksandr.zhydok@it-capital.com.ua>
2022-05-19 21:36:48 +0300

    додав Country

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -36,3 +36,22 @@ create table KDP_COMPANY (
     primary key (ID)
 )^
 -- end KDP_COMPANY
+-- begin KDP_COUNTRY
+create table KDP_COUNTRY (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255) not null,
+    CODE_NUMERIC integer not null,
+    CODE_ALPHA3 varchar(3),
+    CODE_ALPHA2 varchar(2),
+    --
+    primary key (ID)
+)^
+-- end KDP_COUNTRY
