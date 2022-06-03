15e0a8f9c3d27ea5c00904c40bc478ee3774fea8
andrey.artemchuk <artemchuk.a@gmail.com>
2022-06-03 15:22:56 +0300

    Новая сущность "Статусы согласования" Поправки в формах

--- modules/global/src/com/itk/kdp/entity/ReconciliationStatuses.java
+++ modules/global/src/com/itk/kdp/entity/ReconciliationStatuses.java
@@ -17,7 +17,6 @@ public class ReconciliationStatuses extends StandardEntity {
     @Column(name = "NAME", nullable = false)
     @NotNull
     private String name;
-
     public String getName() {
         return name;
     }
