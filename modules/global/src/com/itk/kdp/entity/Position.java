f3c4f53f28f106c4a1ed52cff1bef9e0d09d7311
andrey.artemchuk <artemchuk.a@gmail.com>
2022-06-09 16:36:20 +0300

    Task 2809: створення нових реквізитів сутності "Посади" в елементі та на формі

--- modules/global/src/com/itk/kdp/entity/Position.java
+++ modules/global/src/com/itk/kdp/entity/Position.java
@@ -6,6 +6,7 @@ import com.haulmont.cuba.core.entity.StandardEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Table(name = "KDP_POSITION")
 @Entity(name = "kdp_Position")
@@ -15,6 +16,56 @@ public class Position extends StandardEntity {
 
     @Column(name = "NAME")
     private String name;
+    @Column(name = "POSITION_EN", nullable = false)
+    private String positionEn;
+    @Column(name = "POSITION_UA", nullable = false)
+    private String positionUa;
+    @Column(name = "POSITION_RU", nullable = false)
+    private String positionRu;
+    @Column(name = "POSITION_ID", nullable = false)
+    private UUID positionId;
+    @Column(name = "COMPANY_ID", nullable = false)
+    private UUID companyId;
+
+    public UUID getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(UUID companyId) {
+        this.companyId = companyId;
+    }
+
+    public UUID getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(UUID positionId) {
+        this.positionId = positionId;
+    }
+
+    public String getPositionRu() {
+        return positionRu;
+    }
+
+    public void setPositionRu(String positionRu) {
+        this.positionRu = positionRu;
+    }
+
+    public String getPositionUa() {
+        return positionUa;
+    }
+
+    public void setPositionUa(String positionUa) {
+        this.positionUa = positionUa;
+    }
+
+    public String getPositionEn() {
+        return positionEn;
+    }
+
+    public void setPositionEn(String positionEn) {
+        this.positionEn = positionEn;
+    }
 
     public String getName() {
         return name;
